1ab362e33faf025eb99a26bf6f95cc1f8a28c7dd
Cristian Andres Paolini Vattuone <cristian.paolini@softtek.com>
2023-07-26 14:46:07 +0000

    Menú Popover y validaciones

---
+++ Rules/CRUD/ValidarTelYMail.js
@@ -0,0 +1,35 @@
+/**
+ * Describe this function...
+ * @param {IClientAPI} clientAPI
+ */
+export default function ValidarTelYMail(clientAPI) {
+    try {
+        let oTelefono = clientAPI.evaluateTargetPath('#Page:CrearProveedor/#Control:InpTelefono/#Value').getValue();
+        const regexCelular = /^(15|11)\d{8}$/,
+            regexTelefono = /^(011)?\d{8}$/;
+
+        let telefonoValido = oTelefono.match(regexCelular) || oTelefono.match(regexTelefono);
+
+        if (telefonoValido) {
+            // Teléfono válido, ahora validar email
+            let oEmail = clientAPI.evaluateTargetPath('#Page:CrearProveedor/#Control:InpEmail/#Value').getValue();
+            const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+            let emailValido = oEmail.match(regexEmail);
+
+            if (emailValido) {
+                // Teléfono y email son válidos, ejecutar acción para crear proveedor
+                clientAPI.executeAction('/Proveedores/Actions/CRUD/CrearProveedor.action');
+            } else {
+                clientAPI.executeAction('/Proveedores/Actions/CRUD/ErrorValidarEmail.action');
+                // alert("El email ingresado no es válido.");
+            }
+        } else {
+            // alert("El teléfono ingresado no es válido.");
+            clientAPI.executeAction('/Proveedores/Actions/CRUD/ErrorValidarTelefono.action');
+        }
+    } catch (error) {
+        alert(error);
+    }
+
+}
